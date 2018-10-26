d54971860973b8bd28232387bcb8c59372279a13
Martin Biel <martinbiel93@gmail.com>
2018-10-26 11:08:34 +0200

    Test master, but skip structured unregistered solvers.

--- test/run_dtests.jl
+++ test/run_dtests.jl
@@ -27,53 +27,52 @@ problems = Vector{Tuple{JuMP.Model,SPResult,String}}()
 include("simple.jl")
 @info "Loading farmer..."
 include("farmer.jl")
-
-@info "Test problems loaded. Starting test sequence."
-@testset "Distributed Sanity Check: $name" for (sp,res,name) in problems
-    solve(sp)
-    sp_nondist = StochasticProgram(first_stage_data(sp),second_stage_data(sp),scenarios(sp),procs=[1])
-    transfer_model!(stochastic(sp_nondist),stochastic(sp))
-    generate!(sp_nondist)
-    solve(sp_nondist,solver=GLPKSolverLP())
-    @test scenariotype(sp) == scenariotype(sp_nondist)
-    @test abs(probability(sp)-probability(sp_nondist)) <= 1e-6
-    @test nscenarios(sp) == nscenarios(sp)
-    @test nsubproblems(sp) == nsubproblems(sp_nondist)
-    @test norm(optimal_decision(sp)-optimal_decision(sp_nondist)) <= 1e-6
-    @test abs(optimal_value(sp)-optimal_value(sp_nondist)) <= 1e-6
-end
-
-@testset "Distributed SP Constructs: $name" for (sp,res,name) in problems
-    solve(sp)
-    @test norm(optimal_decision(sp)-res.x̄) <= 1e-2
-    @test abs(optimal_value(sp)-res.VRP) <= 1e-2
-    @test abs(EWS(sp)-res.EWS) <= 1e-2
-    @test abs(EVPI(sp)-res.EVPI) <= 1e-2
-    @test abs(VSS(sp)-res.VSS) <= 1e-2
-    @test abs(EV(sp)-res.EV) <= 1e-2
-    @test abs(EEV(sp)-res.EEV) <= 1e-2
-end
-
-@testset "Distributed Inequalities: $name" for (sp,res,name) in problems
-    @test EWS(sp) <= VRP(sp)
-    @test VRP(sp) <= EEV(sp)
-    @test VSS(sp) >= 0
-    @test EVPI(sp) >= 0
-    @test VSS(sp) <= EEV(sp)-EV(sp)
-    @test EVPI(sp) <= EEV(sp)-EV(sp)
-end
-
 @info "Preparing simple sampler..."
 include("sampling.jl")
-@testset "Distributed Sampling" begin
-    @test nscenarios(sampled_sp) == 0
-    @test nsubproblems(sampled_sp) == 0
-    sample!(sampled_sp,100)
-    @test nscenarios(sampled_sp) == 100
-    @test nsubproblems(sampled_sp) == 100
-    @test abs(probability(sampled_sp)-1.0) <= 1e-6
-    sample!(sampled_sp,100)
-    @test nscenarios(sampled_sp) == 200
-    @test nsubproblems(sampled_sp) == 200
-    @test abs(probability(sampled_sp)-1.0) <= 1e-6
+@info "Test problems loaded. Starting test sequence."
+
+@testset "Distributed Stochastic Programs" begin
+    @testset "Distributed Sanity Check: $name" for (sp,res,name) in problems
+        solve(sp)
+        sp_nondist = StochasticProgram(first_stage_data(sp),second_stage_data(sp),scenarios(sp),procs=[1])
+        transfer_model!(stochastic(sp_nondist),stochastic(sp))
+        generate!(sp_nondist)
+        solve(sp_nondist,solver=GLPKSolverLP())
+        @test scenariotype(sp) == scenariotype(sp_nondist)
+        @test abs(probability(sp)-probability(sp_nondist)) <= 1e-6
+        @test nscenarios(sp) == nscenarios(sp)
+        @test nsubproblems(sp) == nsubproblems(sp_nondist)
+        @test norm(optimal_decision(sp)-optimal_decision(sp_nondist)) <= 1e-6
+        @test abs(optimal_value(sp)-optimal_value(sp_nondist)) <= 1e-6
+    end
+    @testset "Distributed SP Constructs: $name" for (sp,res,name) in problems
+        solve(sp)
+        @test norm(optimal_decision(sp)-res.x̄) <= 1e-2
+        @test abs(optimal_value(sp)-res.VRP) <= 1e-2
+        @test abs(EWS(sp)-res.EWS) <= 1e-2
+        @test abs(EVPI(sp)-res.EVPI) <= 1e-2
+        @test abs(VSS(sp)-res.VSS) <= 1e-2
+        @test abs(EV(sp)-res.EV) <= 1e-2
+        @test abs(EEV(sp)-res.EEV) <= 1e-2
+    end
+    @testset "Distributed Inequalities: $name" for (sp,res,name) in problems
+        @test EWS(sp) <= VRP(sp)
+        @test VRP(sp) <= EEV(sp)
+        @test VSS(sp) >= 0
+        @test EVPI(sp) >= 0
+        @test VSS(sp) <= EEV(sp)-EV(sp)
+        @test EVPI(sp) <= EEV(sp)-EV(sp)
+    end
+    @testset "Distributed Sampling" begin
+        @test nscenarios(sampled_sp) == 0
+        @test nsubproblems(sampled_sp) == 0
+        sample!(sampled_sp,100)
+        @test nscenarios(sampled_sp) == 100
+        @test nsubproblems(sampled_sp) == 100
+        @test abs(probability(sampled_sp)-1.0) <= 1e-6
+        sample!(sampled_sp,100)
+        @test nscenarios(sampled_sp) == 200
+        @test nsubproblems(sampled_sp) == 200
+        @test abs(probability(sampled_sp)-1.0) <= 1e-6
+    end
 end
