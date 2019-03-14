0aef69c885d0f2db72744e5e52df3dda20d43a36
Martin Biel <martinbiel93@gmail.com>
2019-03-14 10:55:36 +0100

    Fix testcase

--- test/run_dtests.jl
+++ test/run_dtests.jl
@@ -96,7 +96,7 @@ include("sampling.jl")
         glpk = GLPKSolverLP()
         L, U = confidence_interval(simple_model, sampler, N = 100, M = 10, confidence = 0.95, solver = glpk)
         @test L <= U
-        saa = SAA(simple_model, sampler, 1000)
+        saa = SAA(simple_model, sampler, 100)
         optimize!(saa, solver = glpk)
         x̂ = optimal_decision(saa)
         Q,_ = evaluate_decision(simple_model, x̂, sampler, solver = glpk)
