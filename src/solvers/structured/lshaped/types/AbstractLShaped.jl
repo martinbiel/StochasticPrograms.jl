c5d2bafbfc157db15ef4f6e7e4c5f5d49dded947
Martin Biel <martinbiel93@gmail.com>
2022-04-16 21:18:24 +0200

    Handle singlevariable/decision objectives in L-shaped.

--- src/solvers/structured/lshaped/types/AbstractLShaped.jl
+++ src/solvers/structured/lshaped/types/AbstractLShaped.jl
@@ -59,6 +59,11 @@ function prepare_master!(lshaped::AbstractLShaped)
         # Cache the objective function
         F = MOI.get(lshaped.master, MOI.ObjectiveFunctionType())
         lshaped.data.master_objective = MOI.get(lshaped.master, MOI.ObjectiveFunction{F}())
+        if F <: MOI.SingleVariable ||  F <: SingleDecision
+            # Set objective to AffineDecisionFunction during procedure
+            F = AffineDecisionFunction{Float64}
+            MOI.set(lshaped.master, MOI.ObjectiveFunction{F}(), convert(F, lshaped.data.master_objective))
+        end
     end
     # Initialize the required number of master variables. Use
     # MOI.VariableIndex(0) as an undef value
